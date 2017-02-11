e2675403811a510e9121a78a12402ce4b8fa2f42
shaojung <shaojung@gmail.com>
2017-02-11 09:43:24 +0800

    toast checked items.

--- app/src/main/java/com/example/student/db020404/MainActivity.java
+++ app/src/main/java/com/example/student/db020404/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.student.db020404;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String data[] = {"AA","BB", "CCC", "DDDD", "EE", "FF", "GGG", "HH", "III", "JJJ", "KKK"};
@@ -15,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter adapter = new MyAdapter(MainActivity.this, data, b);
         lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Show");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i;
+        StringBuilder sb = new StringBuilder();
+        for (i=0;i<data.length;i++)
+        {
+            if (b[i])
+            {
+                sb.append(data[i] + ",");
+            }
+        }
+        Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
